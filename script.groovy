ad8f3af9e33c2919deb0d88f7dc20773418d4589
VarunAkuthota <31569364+VarunAkuthota@users.noreply.github.com>
2022-08-03 20:16:29 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,4 @@
-def buildJar() {
+    def buildJar() {
     echo "building the application..."
     sh 'mvn package'
 } 
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t varunakuthota/itsmyweb:v1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push varunakuthota/itsmyweb:v1'
     }
 } 
 
